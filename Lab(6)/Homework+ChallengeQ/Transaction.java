bcc6f5a0471c448923c3d182b69f9b7c46412518
Riyadh Alzahrani <48260425+RmaDXy@users.noreply.github.com>
2019-04-07 17:39:34 +0300

    Create Transaction.java

---
+++ Lab(6)/Homework+ChallengeQ/Transaction.java
@@ -0,0 +1,70 @@
+package testaccount;
+
+import java.util.Date;
+
+public class Transaction{
+    private Date date;
+    private char type; //D, W
+    
+    private double amount;
+    private double balance;
+    private String description;
+    
+    
+    public Transaction(char type,double amount,double balance,String description){
+        this.date = new Date();
+        this.type = type;
+        this.amount =amount;
+        this.balance = balance;
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public char getType() {
+        return type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setType(char type) {
+        this.type = type;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public String toString(){
+        return "Date: "+date +description +" Amount: $"+amount +" Balance: $"+balance;
+    }
+    
+    
+    
+    
+    
+}

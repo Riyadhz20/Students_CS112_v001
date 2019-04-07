68103b5300475ce0216b2520a6d8f08853bb3e1a
Riyadh Alzahrani <48260425+RmaDXy@users.noreply.github.com>
2019-04-07 17:34:14 +0300

    Create Person.java

---
+++ Lab(5)/Homework/Person.java
@@ -0,0 +1,18 @@
+package test2;
+
+public class Person {
+    private String Name;
+    
+    
+    public void setName(String NewName){
+        Name = NewName;
+        
+    }
+    public String getName(){
+        
+        return Name;
+        
+    }
+    
+    
+}
